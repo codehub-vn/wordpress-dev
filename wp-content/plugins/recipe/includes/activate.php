cca8612f41c1fe5b737e16786bf41a5a85104ab9
Bob <bob.tran@contemi.com.vn>
2017-01-24 17:52:22 +0700

    Create DB table

--- wp-content/plugins/recipe/includes/activate.php
+++ wp-content/plugins/recipe/includes/activate.php
@@ -4,4 +4,18 @@ function r_activate_plugin () {
     if (version_compare(get_bloginfo('version'), '4.2', '<')) {
         wp_die(__('You must have a Wordpress verion later than 4.2'));
     }
+
+    global $wpdb;
+    $createSQL = "
+        CREATE TABLE IF NOT EXISTS `" . $wpdb->prefix. "recipe_ratings` (
+          id bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+          recipe_id bigint(20) unsigned NOT NULL,
+          rating float(3, 1) unsigned NOT NULL,
+          user_id varchar(32) NOT NULL,
+          PRIMARY KEY (id)
+        ) ENGINE=InnoDB ". $wpdb->get_charset_collate()." AUTO_INCREMENT=1;
+    ";
+
+    require(ABSPATH . '/wp-admin/includes/upgrade.php');
+    dbDelta($createSQL);
 }
